02a9135f2a9d85dc6a78c6846b9f19e435b1275f
dabin <kdbj12003@gmail.com>
2022-04-14 10:32:31 +0900

    test prac

---
+++ 두개뽑아서더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  var answer = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  let result = [...new Set(answer)];
+  return result.sort((a, b) => a - b);
+}
+let numbers = [5, 0, 2, 7];
+console.log(solution(numbers));
