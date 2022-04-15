79a234e01b91e226dd6131db9c72d7326058352d
dabin <kdbj12003@gmail.com>
2022-04-15 10:52:34 +0900

    first-budget

---
+++ ㅇ.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+  var answer = 0;
+  let count = 0;
+  d.sort((a, b) => a - b);
+  for (let i = 0; i < d.length; i++) {
+    if ((answer += d[i]) <= budget) {
+      count++;
+    }
+  }
+
+  return count;
+}
+let d = [2, 2, 3, 3];
+console.log(solution(d, 10)); //4
